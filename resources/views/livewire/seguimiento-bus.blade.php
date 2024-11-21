1e1ce8c00dd9666537675b4bcbac699086d2bc9d
MilenkaQuisbert <milenkaelisaq95@gmail.com>
2024-11-21 15:22:53 -0400

    123

--- resources/views/livewire/seguimiento-bus.blade.php
+++ resources/views/livewire/seguimiento-bus.blade.php
@@ -10,7 +10,7 @@
     <input type="date" wire:model="end_day" class="form-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
     </div>
-    <button wire:click="fetchMapData">Generar Mapa</button>
+    <button wire:click="fetchMapData" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Generar Mapa</button>
 
     @if ($mapUrl)
         <iframe src="{{ $mapUrl }}" width="100%" height="500px" frameborder="0"></iframe>
